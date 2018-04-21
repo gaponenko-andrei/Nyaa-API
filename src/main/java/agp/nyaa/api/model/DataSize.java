6e6e7995b6f2de30f0a5c6a30f7237ca2ddde1d5
Гапоненко Андрей <agp32.in@gmail.com>
2018-04-21 22:07:19 +0300

    + DataSize

---
+++ src/main/java/agp/nyaa/api/model/DataSize.java
@@ -0,0 +1,21 @@
+package agp.nyaa.api.model;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+@Value
+public class DataSize {
+
+  private int value;
+
+  private Unit unit;
+
+  @RequiredArgsConstructor
+  public enum Unit {
+    BYTE,
+    KILOBYTE,
+    MEGABYTE,
+    GIGABYTE,
+    TERABYTE
+  }
+}
