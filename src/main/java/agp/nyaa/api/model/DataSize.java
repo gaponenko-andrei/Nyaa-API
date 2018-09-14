224301d6f8dc4664379823146858ebb7d1b3a187
Гапоненко Андрей <agp32.in@gmail.com>
2018-09-14 13:51:23 +0300

    DataSize transition to Immutables

--- src/main/java/agp/nyaa/api/model/DataSize.java
+++ src/main/java/agp/nyaa/api/model/DataSize.java
@@ -1,17 +1,36 @@
 package agp.nyaa.api.model;
 
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import lombok.experimental.Accessors;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Immutable;
+import org.immutables.value.Value.Style;
 
-@Value
-@Accessors(fluent = true)
-public class DataSize {
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.immutables.value.Value.Style.ImplementationVisibility.PACKAGE;
 
-  private int value;
-  private Unit unit;
+@Immutable(builder = false)
+@Style(visibility = PACKAGE)
+public abstract class DataSize {
 
-  @RequiredArgsConstructor
+  @Value.Parameter
+  public abstract Float value();
+
+  @Value.Parameter
+  public abstract Unit unit();
+
+
+  public static DataSize of(final Float value, final Unit unit) {
+    validate(value);
+    return ImmutableDataSize.of(value, unit);
+  }
+
+  private static void validate(final Float value) {
+    checkArgument(isValid(value), "Provided data size value of %s is less then zero.", value);
+  }
+
+  private static boolean isValid(final Float value) {
+    return value > 0;
+  }
+
   public enum Unit {
     BYTE,
     KILOBYTE,
