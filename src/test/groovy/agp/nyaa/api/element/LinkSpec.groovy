c4dc76be9db709f5916c2d937a0545934ad95c59
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2018-12-23 09:34:46 +0300

    LinkTest → LinkSpec

---
+++ src/test/groovy/agp/nyaa/api/element/LinkSpec.groovy
@@ -0,0 +1,33 @@
+package agp.nyaa.api.element
+
+import org.jsoup.nodes.Element
+import org.jsoup.parser.Tag
+import spock.lang.Specification
+
+
+class LinkSpec extends Specification {
+
+  def "Constructor should accept link element"() {
+
+    given:
+      final Element linkElement = new Element(Tag.valueOf("a"), "baseUrl")
+
+    when:
+      new Link(linkElement)
+
+    then:
+      notThrown(Exception)
+  }
+
+  def "Constructor should throw on non-link element"() {
+
+    given:
+      final Element nonLinkElement = new Element(Tag.valueOf("tr"), "baseUrl")
+
+    when:
+      new Link(nonLinkElement)
+
+    then:
+      thrown(IllegalArgumentException)
+  }
+}
