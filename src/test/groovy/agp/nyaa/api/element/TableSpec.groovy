1f15955ec7cc539f20b3d35301b0856934563c9a
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2018-12-23 09:39:43 +0300

    TableTest → TableSpec

---
+++ src/test/groovy/agp/nyaa/api/element/TableSpec.groovy
@@ -0,0 +1,33 @@
+package agp.nyaa.api.element
+
+import org.jsoup.nodes.Element
+import org.jsoup.parser.Tag
+import spock.lang.Specification
+
+
+class TableSpec extends Specification {
+
+  def "Constructor should accept table element"() {
+
+    given:
+      final Element tableElement = new Element(Tag.valueOf("table"), "baseUrl")
+
+    when:
+      new Table(tableElement)
+
+    then:
+      notThrown(Exception)
+  }
+
+  def "Constructor should throw on non-table element"() {
+
+    given:
+      final Element nonTableElement = new Element(Tag.valueOf("tr"), "baseUrl")
+
+    when:
+      new Table(nonTableElement)
+
+    then:
+      thrown(IllegalArgumentException)
+  }
+}
