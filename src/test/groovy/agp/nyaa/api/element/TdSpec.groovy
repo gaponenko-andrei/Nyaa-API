162b4df66fd2f5286005424694b14ef01673ecfa
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2018-12-23 09:40:07 +0300

    TdTest → TdSpec

---
+++ src/test/groovy/agp/nyaa/api/element/TdSpec.groovy
@@ -0,0 +1,33 @@
+package agp.nyaa.api.element
+
+import org.jsoup.nodes.Element
+import org.jsoup.parser.Tag
+import spock.lang.Specification
+
+
+class TdSpec extends Specification {
+
+  def "Constructor should accept td element"() {
+
+    given:
+      final Element tdElement = new Element(Tag.valueOf("td"), "baseUrl")
+
+    when:
+      new Td(tdElement)
+
+    then:
+      notThrown(Exception)
+  }
+
+  def "Constructor should throw on non-td element"() {
+
+    given:
+      final Element nonTdElement = new Element(Tag.valueOf("tr"), "baseUrl")
+
+    when:
+      new Td(nonTdElement)
+
+    then:
+      thrown(IllegalArgumentException)
+  }
+}
